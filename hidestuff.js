a86c0b2a907f02704a3ae56d3d67c240bb04b1e0
kendallColeman <kendall.coleman@outlook.com>
2017-09-25 15:14:31 -0500

    new

---
+++ hidestuff.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+          
+        $(".textLabel").hide();
+        $(".text").hide();
+        
+
+       $(".square").hover(function(){
+            
+             $(".textLabel").show( //switch with rand
+             $(this).addClass('.fadeIn').show);
+        });
+                     
+                     
+                     
+                     )
+
+});
