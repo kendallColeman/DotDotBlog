cbe37daa24c1fc8315912987c673f98fdc487c81
kendallColeman <32066126+kendallColeman@users.noreply.github.com>
2017-09-28 08:58:05 -0500

    new files

--- hidestuff.js
+++ hidestuff.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
           
         $(".Label1").hide();
+        $(".Label2").hide();
+        $(".Label3").hide();
         $(".text").hide();
         $("#text").hide();
 
-       $(".square").hover(function(){
+       $(".square1").hover(function(){
             
              $(".Label1").show( //switch with rand
-             $(this).addClass('.fadeIn').show);
+             $(this).addClass('.slideInUp').show);
+             $(".text").show();
+        });
+    $(".square2").hover(function(){
+            
+             $(".Label2").show( //switch with rand
+             $(this).addClass('.slideInUp').show);
+             $(".text").show();
+        });
+        $(".square3").hover(function(){
+            
+             $(".Label3").show( //switch with rand
+             $(this).addClass('.slideInUp').show);
              $(".text").show();
         });
 
